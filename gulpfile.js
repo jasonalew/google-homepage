72f5d8c281d083f4d6e22255e3fbcf17504c77a7
Jason Lew <jason.a.lew@gmail.com>
2015-04-16 16:06:55 -0700

    added watch and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync').create();
+  browserSync = require('browser-sync').create(),
+  autoprefixer = require('gulp-autoprefixer');
 
 var config = {
-    server: '',
-    files: 'css/*.css, index.html'
+  server: '',
+  files: 'dist/*.css, index.html'
 };
 
 gulp.task('browser-sync', function() {
-    browserSync.init(config);
+  browserSync.init(config);
 });
 
-gulp.task('default', ['browser-sync']);
+gulp.task('auto-prefixer', function() {
+  return gulp.src('css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('css/*.css', ['auto-prefixer']);
+});
+
+gulp.task('default', ['browser-sync', 'auto-prefixer', 'watch']);
